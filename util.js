54cb93433e4cbb0d8b5674ca579cfa96a7abe001
sl1673495 <949589855@qq.com>
2020-06-01 23:41:03 +0800

    feat: dp 完成

---
+++ util.js
@@ -0,0 +1,41 @@
+let loadImgHeights = (imgs) => {
+  return new Promise((resolve, reject) => {
+    const length = imgs.length
+    const heights = []
+    let count = 0
+    const load = (index) => {
+      let img = new Image()
+      const checkIfFinished = () => {
+        count++
+        if (count === length) {
+          resolve(heights)
+        }
+      }
+      img.onload = () => {
+        const ratio = img.height / img.width
+        const halfHeight = ratio * halfInnerWidth
+        // 高度按屏幕一半的比例来计算
+        heights[index] = halfHeight
+        checkIfFinished()
+      }
+      img.onerror = () => {
+        heights[index] = 0
+        checkIfFinished()
+      }
+      img.src = imgs[index]
+    }
+    imgs.forEach((img, index) => load(index))
+  })
+}
+
+let sum = (nums) => nums.reduce((a, b) => a + b)
+
+let omitByIndexes = (arr, omitIndexes) => {
+  let res = []
+  for (let i = 0; i < arr.length; i++) {
+    if (!omitIndexes.includes(i)) {
+      res.push(i)
+    }
+  }
+  return res
+}
