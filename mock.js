7d5f2c559c4899ad87b297e844b61d173e0ff056
ssh <949589855@qq.com>
2020-06-02 11:53:54 +0800

    feat: greedy

--- mock.js
+++ mock.js
@@ -1,4 +1,4 @@
-var SISTERS = [
+let SISTERS = [
   'https://pic3.zhimg.com/v2-89735fee10045d51693f1f74369aaa46_r.jpg',
   'https://pic1.zhimg.com/v2-ca51a8ce18f507b2502c4d495a217fa0_r.jpg',
   'https://pic1.zhimg.com/v2-c90799771ed8469608f326698113e34c_r.jpg',
--- util.js
+++ util.js
@@ -39,3 +39,14 @@ let omitByIndexes = (arr, omitIndexes) => {
   }
   return res
 }
+
+let findNearest = (arr, num) => {
+  let prevIndex = 0
+  for (let index in arr) {
+    if (index >= num && prevIndex < num) {
+      return index
+    }
+    prevIndex = index
+  }
+  return num
+}
